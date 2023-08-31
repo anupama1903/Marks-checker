2ba140e08f2105ae3a5c757dc4b85d09921bbb48
anupama1903 <116435676+anupama1903@users.noreply.github.com>
2023-08-31 21:15:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+m=int(input("Enter the value:"))
+if(m>80):
+    print("passed in distinction")
+elif(m>=50 & m<=80):
+    print("passed in first class")
+elif(m>=35 & m<=50):
+    print("passed in second class")
+else:
+    print("fail")
